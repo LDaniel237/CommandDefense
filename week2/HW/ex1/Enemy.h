2cf219101bfd6a43f55173959a73890e44ed5be6
LDaniel237 <lkingdaniel90@gmail.com>
2022-03-06 14:50:29 +0200

    modificat fisiere .h

--- week2/HW/ex1/Enemy.h
+++ week2/HW/ex1/Enemy.h
@@ -9,9 +9,35 @@ private:
     int health;
 
 public:
-    void Init(Point intitialLocation, int initialHealh);
-    void Move(EnemyDirection direction, int step);
+    void Init(Point intitialLocation, int initialHealh) {
+        location = intitialLocation;
+        health = initialHealh;
+    }
+    void Move(EnemyDirection direction, int step) {
+        switch (direction) {
+        case EnemyDirection::UpDirection:
+            location.y -= step;
+            break;
+        case EnemyDirection::DownDirection:
+            location.y += step;
+            break;
+        case EnemyDirection::LeftDirection:
+            location.x -= step;
+            break;
+        case EnemyDirection::RightDirection:
+            location.x += step;
+            break;
+        default:
+            break;
+        }
+    }
 
-    bool IsDead();//returns true is current health is lower or equal to 0
-    void Shoot(int damage); //decrease the health by damage
+    // returns true is current health is lower or equal to 0
+    bool IsDead() {
+        return (health <= 0);
+    }
+    // decrease the health by damage
+    void Shoot(int damage) {
+        health -= damage;
+    } 
 };
